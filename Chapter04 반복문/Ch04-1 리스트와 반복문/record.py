b761244878c3b1e4e179a261d88f885eac111e6b
bitcamp <cldud400@naver.com>
2022-01-07 16:40:47 +0900

    ch04 -2 End

---
+++ Chapter04 반복문/Ch04-1 리스트와 반복문/record.py
@@ -0,0 +1,19 @@
+records = [90, 85, 70, 55, 95, 60, 80, 90, 85, 70, 55, 95, 60, 80, 90, 85, 70, 55, 95, 60, 80]
+l = len(records)
+max_, min_ = -1, 9999
+total_ = 0
+
+
+for score in records:
+    total_ += score
+    if score > max_:
+        max_ = score
+    if score < min_:
+        min_ = score
+        
+avg_ = total_ / l
+
+print('최고점 :', max_)
+print('최저점 :', min_)
+print('총점 :', total_)
+print('평균 :', round(avg_,2))
