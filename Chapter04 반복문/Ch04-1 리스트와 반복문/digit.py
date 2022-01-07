cdf081c445787337f51b5a64b44eed73d04b02d1
bitcamp <cldud400@naver.com>
2022-01-07 17:58:35 +0900

    Ch04 checkpoint

--- Chapter04 반복문/Ch04-1 리스트와 반복문/digit.py
+++ Chapter04 반복문/Ch04-1 리스트와 반복문/digit.py
@@ -3,12 +3,12 @@ numbers = [273, 103, 5, 32, 65, 9, 72, 800, 99, -100, -9999, 9999, 100, 0]
 #273 은(는) 3자리수 입니다.
 
 for i in numbers:
-    num_ = i
-    i = abs(i)
-    n = 0
+    origin_num = i
+    tmp = abs(i)
+    cnt = 0
     while True:
-        i = (i // 10)
-        n += 1     
-        if i == 0:
+        tmp = (tmp // 10)
+        cnt += 1     
+        if tmp == 0:
             break 
-    print(f'{num_}은(는) {n}자리수 입니다.')
+    print(f'{origin_num}은(는) {cnt}자리수 입니다.')
