410dbf327d7af28395f53d67fcb2713b156861db
bitcamp <cldud400@naver.com>
2022-01-10 13:05:37 +0900

    ch04 End

--- Chapter04 반복문/Ch04-1 리스트와 반복문/digit.py
+++ Chapter04 반복문/Ch04-1 리스트와 반복문/digit.py
@@ -10,5 +10,13 @@ for i in numbers:
         if i == 0:
             break
     print(f'{num_}은(는) {cnt}자리수 입니다.')
+    
+    
+# for i in numbers:
+#     i = str(i)
+#     num = len(i)
+#     print(f'{i}는 {num}자리수 입니다.')
+
+
 
 
